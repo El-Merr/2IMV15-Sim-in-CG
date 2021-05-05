78c9b40b28dc32ab8c68ae079641dc994b79ea0e
ThomasVersaevel <Thomas0699@gmail.com>
2021-05-05 17:07:02 +0200

    Changed ForceAccumulator to Force

--- unified_makefile_project1/src/Particle.cpp
+++ unified_makefile_project1/src/Particle.cpp
@@ -3,7 +3,7 @@
 
 Particle::Particle(const Vec2f & ConstructPos) :
 	m_ConstructPos(ConstructPos), m_Position(Vec2f(0.0, 0.0)),
-	m_Velocity(Vec2f(0.0, 0.0)), m_ForceAccumulator(Vec2f(0.0, 0.0)), m_Mass(1)
+	m_Velocity(Vec2f(0.0, 0.0)), m_Force(Vec2f(0.0, 0.0)), m_Mass(1)
 {
 }
 
@@ -15,7 +15,7 @@ void Particle::reset()
 {
 	m_Position = m_ConstructPos;
 	m_Velocity = Vec2f(0.0, 0.0);
-	m_ForceAccumulator = Vec2f(0.0, 0.0);
+	m_Force = Vec2f(0.0, 0.0);
 	m_Mass = 1;
 }
 
