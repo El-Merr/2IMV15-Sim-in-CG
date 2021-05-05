a44abf346a442e719fb3009744e424df2db58b98
Merel <m.a.eikens@student.tue.nl>
2021-05-05 16:44:21 +0200

    Fixed typo shoud -> should

--- unified_makefile_project1/src/ParticleToy.cpp
+++ unified_makefile_project1/src/ParticleToy.cpp
@@ -86,7 +86,7 @@ static void init_system(void)
 	pVector.push_back(new Particle(center + offset + offset));
 	pVector.push_back(new Particle(center + offset + offset + offset));
 	
-	// You shoud replace these with a vector generalized forces and one of
+	// You should replace these with a vector generalized forces and one of
 	// constraints...
 	delete_this_dummy_spring = new SpringForce(pVector[0], pVector[1], dist, 1.0, 1.0);
 	delete_this_dummy_rod = new RodConstraint(pVector[1], pVector[2], dist);
