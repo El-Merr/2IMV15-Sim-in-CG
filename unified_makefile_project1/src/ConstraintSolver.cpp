d38e892b25dcfe7ae576267cf81d4035d6f7d82e
Merel <m.a.eikens@student.tue.nl>
2021-05-20 17:36:59 +0200

    Cleaned up code

--- unified_makefile_project1/src/ConstraintSolver.cpp
+++ unified_makefile_project1/src/ConstraintSolver.cpp
@@ -26,18 +26,6 @@ void ConstraintSolver::apply_constraint()
     MatrixXf Jt = MatrixXf::Zero(vector_size, num_constraints);
     MatrixXf J_deriv = MatrixXf::Zero(num_constraints, vector_size);
 
-//
-//    Vec2f q = Vec2f(0.0, 0.0);
-//    Vec2f Q = Vec2f(0.0, 0.0);
-//    Mat2 W = Mat2();
-//
-//    Vec2f C = Vec2f(0.0, 0.0);
-//    VectorXf Cder = VectorXf::Zero(constraintsLength);
-////    MatrixXf J = MatrixXf::Zero(constraintsLength, dimensions);
-////    MatrixXf Jt = MatrixXf::Zero(dimensions, constraintsLength);
-//    MatrixXf Jder = MatrixXf::Zero(constraintsLength, dimensions);
-//
-//
     // Fill W, Q and q with all particles
     for ( int p = 0; p < m_pVector.size(); p++) {
         Particle* particle = m_pVector[p];
@@ -81,53 +69,23 @@ void ConstraintSolver::apply_constraint()
     MatrixXf JWJt = JW * Jt;
     VectorXf Jdq = -1 * J_deriv * q;
     VectorXf JWQ = JW * Q;
-//    RowVectorXf JWQt = JW * Q.transpose();
-//    VectorXf temp = -Jdq.transpose() - JWQ.transpose();
-    VectorXf x = Jdq - JWQ - C - C_deriv;
 
+    VectorXf JWJt_lambda = Jdq - JWQ - C - C_deriv;
+
+    // use linear Conjugate Gradient to solve x for Ax=b
+    // where JWJt * lambda = JWJt_lambda
     ConjugateGradient<MatrixXf, Lower|Upper> cg;
     cg.compute(JWJt);
-    auto lambda = cg.solve(x);
-//    auto lambda = ConjGrad();
+    auto lambda = cg.solve(JWJt_lambda);
 
-//    VectorXf lambda_t = JWJt.inverse() * ( -Jdq.transpose() - JWQ.transpose());
+    // calculate the force to compensate illegal movement
     VectorXf Q_hat = Jt * lambda;
 
-//
+    // apply Q_hat for every particle
     for ( int p = 0; p < m_pVector.size(); p++ ) {
         Particle* particle = m_pVector[p];
         particle->m_Force[0] += Q_hat[p*dimensions];
         particle->m_Force[1] += Q_hat[p*dimensions + 1];
     }
-
-//    VectorXf KsC = ks * C;
-//    VectorXf KdCd = kd * Cder;
-//    VectorXf rhs = Jderq - JWQ - KsC - KdCd;
-
-//
-////    Mat2 JW = Mat2(J[0]*W[0][0] + J[0]*W[0][1], J[0]*W[1][0] + J[0]*W[1][1],
-////                   J[1]*W[0][0] + J[1]*W[0][1], J[1]*W[1][0] + J[1]*W[1][1]);
-////
-//////    Mat2 JWJ_t = Mat2(JW[0] * J[0],  JW[1] * J[0], //j * j_transposed
-//////                      JW[0] * J[1], JW[1] * J[1]);
-////
-////    Mat2 JWJ_t = MatVec_tMult(JW, J);
-////
-////    Vec2f J_dq = J_derivative * q;
-////
-////    Mat2 JWQ_t = transpose(MatVec_tMult(JW, Q));
-////
-////    Vec2f lambda_t = inverse(JWJ_t) * JWQ_t; //(-J_dq - JWQ_t);
-////
-////    Vec2f Q_hat = lambda_t_t * J;
-////
-////    m_p->m_Force += Q_hat;
-//
-////    float p_r2 = pow(m_p->m_Force[0],2) + pow(m_p->m_Force[0],2);
-////    float r_scale = pow(m_radius, 2) / p_r2;
-////
-////    m_p->m_Force[0] = sqrt(pow(m_p->m_Force[0],2) / r_scale);
-////    m_p->m_Force[1] = sqrt(pow(m_p->m_Force[1],2) / r_scale);
-//
 }
 
