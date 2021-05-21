76cc1196eba7565287674490bab7f233edd330a6
Merel <m.a.eikens@student.tue.nl>
2021-05-22 01:10:21 +0200

    Fixed Rod constraint math

--- unified_makefile_project1/src/RodConstraint.cpp
+++ unified_makefile_project1/src/RodConstraint.cpp
@@ -25,29 +25,34 @@ std::vector<Particle*> RodConstraint::getParticles()
 
 float RodConstraint::constraint()
 {
-    Vec2f pos_diff = m_p2->m_Position - m_p1->m_Position;
+    Vec2f pos_diff = m_p1->m_Position - m_p2->m_Position;
     return pow(pos_diff[0], 2) + pow(pos_diff[1], 2) - pow(m_dist, 2);
 }
 
 float RodConstraint::constraint_derivative()
 {
-    Vec2f pos_diff = m_p2->m_Position - m_p1->m_Position;
-    return 2 * pos_diff * m_p2->m_Velocity;
+    Vec2f pos_diff = m_p1->m_Position - m_p2->m_Position;
+    return 2 * pos_diff * 2 * (m_p1->m_Velocity - m_p2->m_Velocity);
 }
 
 std::vector<Vec2f> RodConstraint::J()
 {
-    Vec2f pos_diff = m_p2->m_Position - m_p1->m_Position;
-    Vec2f j = pos_diff * 2;
+    Vec2f pos_diff1 = m_p1->m_Position - m_p2->m_Position;
+    Vec2f pos_diff2 = m_p2->m_Position - m_p1->m_Position;
+    Vec2f j_p1 = pos_diff1 * 2;
+    Vec2f j_p2 = pos_diff2 * 2;
     std::vector<Vec2f> res;
-    res.push_back(j);
+    res.push_back(j_p1);
+    res.push_back(j_p2);
     return res;
 }
 
 std::vector<Vec2f> RodConstraint::J_derivative()
 {
-    Vec2f j_deriv = m_p2->m_Velocity * 2;
+    Vec2f j_deriv1 = (m_p1->m_Velocity - m_p2->m_Velocity) * 2;
+    Vec2f j_deriv2 = (m_p2->m_Velocity - m_p1->m_Velocity) * 2;
     std::vector<Vec2f> res;
-    res.push_back(j_deriv);
+    res.push_back(j_deriv1);
+    res.push_back(j_deriv2);
     return res;
 }
