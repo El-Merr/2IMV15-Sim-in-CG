90e754fc3db639d7ea3a02f302bae95ca12b69ea
Merel <m.a.eikens@student.tue.nl>
2021-05-21 15:37:04 +0200

    Added rod constraint

--- unified_makefile_project1/src/RodConstraint.cpp
+++ unified_makefile_project1/src/RodConstraint.cpp
@@ -14,3 +14,40 @@ void RodConstraint::draw()
   glEnd();
 
 }
+
+std::vector<Particle*> RodConstraint::getParticles()
+{
+    std::vector<Particle*> particles;
+    particles.push_back(m_p1);
+    particles.push_back(m_p2);
+    return particles;
+}
+
+float RodConstraint::constraint()
+{
+    Vec2f pos_diff = m_p2->m_Position - m_p1->m_Position;
+    return pow(pos_diff[0], 2) + pow(pos_diff[1], 2) - pow(m_dist, 2);
+}
+
+float RodConstraint::constraint_derivative()
+{
+    Vec2f pos_diff = m_p2->m_Position - m_p1->m_Position;
+    return 2 * pos_diff * m_p2->m_Velocity;
+}
+
+std::vector<Vec2f> RodConstraint::J()
+{
+    Vec2f pos_diff = m_p2->m_Position - m_p1->m_Position;
+    Vec2f j = pos_diff * 2;
+    std::vector<Vec2f> res;
+    res.push_back(j);
+    return res;
+}
+
+std::vector<Vec2f> RodConstraint::J_derivative()
+{
+    Vec2f j_deriv = m_p2->m_Velocity * 2;
+    std::vector<Vec2f> res;
+    res.push_back(j_deriv);
+    return res;
+}
