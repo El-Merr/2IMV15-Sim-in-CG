bf13d2090181a66206dec2e4a41d1240af935641
ThomasVersaevel <Thomas0699@gmail.com>
2021-06-02 18:27:38 +0200

    Copied many things from demo.c to particle toy

--- unified_makefile_project1/src/ParticleToy.cpp
+++ unified_makefile_project1/src/ParticleToy.cpp
@@ -19,11 +19,16 @@
 #include <iostream>
 
 
+#define IX(i,j) ((i)+(N+2)*(j))
+
 /* macros */
 
 /* external definitions (from solver) */
 extern void simulation_step( std::vector<Particle*> pVector, float dt, bool slomoBool, int scheme );
 /* global variables */
+// demo.c these are in solver.c
+extern void dens_step ( int N, float * x, float * x0, float * u, float * v, float diff, float dt );
+extern void vel_step ( int N, float * u, float * v, float * u0, float * v0, float visc, float dt );
 
 static int N;
 static float dt, d;
@@ -32,6 +37,14 @@ static int dump_frames;
 static int frame_number;
 static int mouse_particle_index;
 static int scheme;
+
+// New from demo.c
+static float diff, visc;
+static float force, source;
+static int dvel;
+static float * u, * v, * u_prev, * v_prev;
+static float * dens, * dens_prev;
+
 
 // spring constants
 static float spring_ks = 0.01;
@@ -102,6 +115,13 @@ static void free_data ( void )
         delete wall;
         wall = NULL;
     }
+    //from demo.c
+    if ( u ) free ( u );
+    if ( v ) free ( v );
+    if ( u_prev ) free ( u_prev );
+    if ( v_prev ) free ( v_prev );
+    if ( dens ) free ( dens );
+    if ( dens_prev ) free ( dens_prev );
 }
 
 static void clear_data ( void )
@@ -111,6 +131,13 @@ static void clear_data ( void )
 	for(ii=0; ii<size; ii++){
 		pVector[ii]->reset();
 	}
+
+	//from demo.c
+    int i, size2=(N+2)*(N+2);
+
+    for ( i=0 ; i<size2 ; i++ ) {
+        u[i] = v[i] = u_prev[i] = v_prev[i] = dens[i] = dens_prev[i] = 0.0f;
+    }
 }
 
 static void clear_forces ( void )
@@ -121,6 +148,28 @@ static void clear_forces ( void )
         pVector[ii]->clear_force();
     }
 }
+/**
+ * From demo.c
+ */
+static int allocate_data ( void )
+{
+    int size = (N+2)*(N+2);
+
+    u			= (float *) malloc ( size*sizeof(float) );
+    v			= (float *) malloc ( size*sizeof(float) );
+    u_prev		= (float *) malloc ( size*sizeof(float) );
+    v_prev		= (float *) malloc ( size*sizeof(float) );
+    dens		= (float *) malloc ( size*sizeof(float) );
+    dens_prev	= (float *) malloc ( size*sizeof(float) );
+
+    if ( !u || !v || !u_prev || !v_prev || !dens || !dens_prev ) {
+        fprintf ( stderr, "cannot allocate data\n" );
+        return ( 0 );
+    }
+
+    return ( 1 );
+}
+
 /**
  * Handles mouse interaction during simulation
  * When the left mouse button is held down a spring force between
@@ -193,25 +242,7 @@ static void init_system(int sceneNr)
 
 	switch(sceneNr) {
         case 0: {//default scene
-            // Create three particles, attach them to each other, then add a
-            // circular wire constraint to the first.
-            pVector.push_back(new Particle(center + offset, defaultMass));
-            pVector.push_back(new Particle(center + offset + offset, defaultMass));
-            pVector.push_back(new Particle(center + offset + offset + offset, defaultMass));
 
-            mouse_particle_index = 1; // sets the 2nd particle to be the mouse interaction particle.
-
-            springForce.push_back(new SpringForce(pVector[0], pVector[1], dist, spring_ks, spring_kd));
-
-            // constraints
-            rodConstraint = new RodConstraint(pVector[1], pVector[2], dist);
-            circularWireConstraint = new CircularWireConstraint(pVector[0], center, dist);
-
-            constraints.push_back(circularWireConstraint);
-            constraints.push_back(rodConstraint);
-            constraintSolver = new ConstraintSolver(pVector, constraints);
-
-            break;
         }
 
         case 1: { //cloth scene
@@ -291,6 +322,26 @@ static void init_system(int sceneNr)
 
                 break;
         }
+        case 4:
+        {// Create three particles, attach them to each other, then add a
+            // circular wire constraint to the first.
+            pVector.push_back(new Particle(center + offset, defaultMass));
+            pVector.push_back(new Particle(center + offset + offset, defaultMass));
+            pVector.push_back(new Particle(center + offset + offset + offset, defaultMass));
+
+            mouse_particle_index = 1; // sets the 2nd particle to be the mouse interaction particle.
+
+            springForce.push_back(new SpringForce(pVector[0], pVector[1], dist, spring_ks, spring_kd));
+
+            // constraints
+            rodConstraint = new RodConstraint(pVector[1], pVector[2], dist);
+            circularWireConstraint = new CircularWireConstraint(pVector[0], center, dist);
+
+            constraints.push_back(circularWireConstraint);
+            constraints.push_back(rodConstraint);
+            constraintSolver = new ConstraintSolver(pVector, constraints);
+
+            break;}
     }
     gravityForce = new GravityForce(pVector);
 }
@@ -351,6 +402,64 @@ static void draw_particles ( void )
 	{
 		pVector[ii]->draw();
 	}
+}
+/**
+ * From demo.c
+ */
+static void draw_velocity ( void )
+{
+    int i, j;
+    float x, y, h;
+
+    h = 1.0f/N;
+
+    glColor3f ( 1.0f, 1.0f, 1.0f );
+    glLineWidth ( 1.0f );
+
+    glBegin ( GL_LINES );
+
+    for ( i=1 ; i<=N ; i++ ) {
+        x = (i-0.5f)*h;
+        for ( j=1 ; j<=N ; j++ ) {
+            y = (j-0.5f)*h;
+
+            glVertex2f ( x, y );
+            glVertex2f ( x+u[IX(i,j)], y+v[IX(i,j)] );
+        }
+    }
+
+    glEnd ();
+}
+/**
+ * From demo.c
+ */
+static void draw_density ( void )
+{
+    int i, j;
+    float x, y, h, d00, d01, d10, d11;
+
+    h = 1.0f/N;
+
+    glBegin ( GL_QUADS );
+
+    for ( i=0 ; i<=N ; i++ ) {
+        x = (i-0.5f)*h;
+        for ( j=0 ; j<=N ; j++ ) {
+            y = (j-0.5f)*h;
+
+            d00 = dens[IX(i,j)];
+            d01 = dens[IX(i,j+1)];
+            d10 = dens[IX(i+1,j)];
+            d11 = dens[IX(i+1,j+1)];
+
+            glColor3f ( d00, d00, d00 ); glVertex2f ( x, y );
+            glColor3f ( d10, d10, d10 ); glVertex2f ( x+h, y );
+            glColor3f ( d11, d11, d11 ); glVertex2f ( x+h, y+h );
+            glColor3f ( d01, d01, d01 ); glVertex2f ( x, y+h );
+        }
+    }
+
+    glEnd ();
 }
 
 static void draw_forces ( void )
@@ -413,20 +522,36 @@ relates mouse movements to particle toy construction
 ----------------------------------------------------------------------
 */
 
-static void get_from_UI ()
+static void get_from_UI (float * d, float * u, float * v)
 {
-	int i, j;
+	int i, j, size = (N+2)*(N+2);
 	// int size, flag;
 //	int hi, hj;
 //	float x, y;
 
+    for ( i=0 ; i<size ; i++ ) {
+        u[i] = v[i] = d[i] = 0.0f;
+    }
+
 	if ( !mouse_down[0] && !mouse_down[2] && !mouse_release[0] 
 	&& !mouse_shiftclick[0] && !mouse_shiftclick[2] ) return;
 
-	i = (int)((       mx /(float)win_x)*N);
-	j = (int)(((win_y-my)/(float)win_y)*N);
+    //from demo.c
+    i = (int)((       mx /(float)win_x)*N+1);
+    j = (int)(((win_y-my)/(float)win_y)*N+1);
 
-	if ( i<1 || i>N || j<1 || j>N ) return;
+    if ( i<1 || i>N || j<1 || j>N ) return;
+
+    if ( mouse_down[0] ) {
+        u[IX(i,j)] = force * (mx-omx);
+        v[IX(i,j)] = force * (omy-my);
+    }
+
+    if ( mouse_down[2] ) {
+        d[IX(i,j)] = source;
+    }
+    //end mouse from demo.c
+
 
 //    x = (float)2 * i / N - 1;
 //    y = (float)2 * j / N - 1;
@@ -495,6 +620,12 @@ static void key_func ( unsigned char key, int x, int y )
         case 'S':
             slomo = !slomo;
             break;
+        // from demo.c
+        case 'v':
+        case 'V':
+            dvel = !dvel;
+            break;
+
 
 //        case 'z': //crude collision mode
 //        case 'Z':
@@ -523,6 +654,10 @@ static void key_func ( unsigned char key, int x, int y )
         case '4':
             free_data();
             init_system(3);
+            break;
+        case '5':
+            free_data();
+            init_system(4);
             break;
 
         case 'e':
@@ -577,9 +712,14 @@ static void idle_func ( void )
         simulation_step( pVector, dt, slomo, scheme );
 	}
 	else {
-	    get_from_UI();
+        // demo.c
+        get_from_UI ( dens_prev, u_prev, v_prev );
+        vel_step ( N, u, v, u_prev, v_prev, visc, dt );
+        dens_step ( N, dens, dens_prev, u, v, diff, dt );
 	    remap_GUI();
 	}
+
+
 
 	glutSetWindow ( win_id );
 	glutPostRedisplay ();
@@ -601,6 +741,9 @@ static void display_func ( void )
 	    mouseParticle->draw();
 	    mouseForce->draw();
 	}
+	// demo.c
+    if ( dvel ) draw_velocity ();
+    else		draw_density ();
 
 	post_display ();
 }
@@ -685,7 +828,49 @@ int main ( int argc, char ** argv )
 	frame_number = 0;
 	scheme = 2;
 
-	init_system(0);
+	//init_system(0);
+    if ( argc != 1 && argc != 6 ) {
+        fprintf ( stderr, "usage : %s N dt diff visc force source\n", argv[0] );
+        fprintf ( stderr, "where:\n" );\
+		fprintf ( stderr, "\t N      : grid resolution\n" );
+        fprintf ( stderr, "\t dt     : time step\n" );
+        fprintf ( stderr, "\t diff   : diffusion rate of the density\n" );
+        fprintf ( stderr, "\t visc   : viscosity of the fluid\n" );
+        fprintf ( stderr, "\t force  : scales the mouse movement that generate a force\n" );
+        fprintf ( stderr, "\t source : amount of density that will be deposited\n" );
+        exit ( 1 );
+    }
+
+    if ( argc == 1 ) {
+        N = 64;
+        dt = 0.1f;
+        diff = 0.0f;
+        visc = 0.0f;
+        force = 5.0f;
+        source = 100.0f;
+        fprintf ( stderr, "Using defaults : N=%d dt=%g diff=%g visc=%g force = %g source=%g\n",
+                  N, dt, diff, visc, force, source );
+    } else {
+        N = atoi(argv[1]);
+        dt = atof(argv[2]);
+        diff = atof(argv[3]);
+        visc = atof(argv[4]);
+        force = atof(argv[5]);
+        source = atof(argv[6]);
+    }
+
+    printf ( "\n\nHow to use this demo:\n\n" );
+    printf ( "\t Add densities with the right mouse button\n" );
+    printf ( "\t Add velocities with the left mouse button and dragging the mouse\n" );
+    printf ( "\t Toggle density/velocity display with the 'v' key\n" );
+    printf ( "\t Clear the simulation by pressing the 'c' key\n" );
+    printf ( "\t Quit by pressing the 'q' key\n" );
+
+    dvel = 0;
+//
+//    if ( !allocate_data () ) exit ( 1 );
+//    clear_data ();
+
 	
 	win_x = 800;
 	win_y = 800;
