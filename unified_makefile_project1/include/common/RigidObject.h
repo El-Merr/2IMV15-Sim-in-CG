783d0f4a868c21e8b06b282bda2a9d1e5ce9cffa
Merel <m.a.eikens@student.tue.nl>
2021-06-19 21:37:56 +0200

    Added Particles as rigidbody points

--- unified_makefile_project1/include/common/RigidObject.h
+++ unified_makefile_project1/include/common/RigidObject.h
@@ -6,6 +6,7 @@
 #include <Eigen/IterativeLinearSolvers>
 #include <gfx/vec2.h>
 #include "Object.h"
+#include "Particle.h"
 
 class RigidObject : public Object {
     public:
@@ -15,22 +16,38 @@ class RigidObject : public Object {
         void draw_object() override;
         std::vector<Vec2f> get_points() override;
 
+        std::vector<Vec2f> derive_eval();
+
+        void set_state(Vec2f pos, Vec2f vel);
+        std::vector<Vec2f> get_state();
 
         void clear_force();
         void reset();
-        void computeBodySpace();
 
-//        void drawRigidObject();
-        std::vector<Vec2f> derive_eval();
-        void set_state(Vec2f pos, Vec2f vel);
-        std::vector<Vec2f> get_state();
-        Vec2f center;
-        Vec2f m_Velocity;
-        Vec2f m_Force;
-        float m_Mass;
-        std::vector<Vec2f> m_Direction;
     private:
-        std::vector<Vec2f> points;
+
+        Particle* pCenter;
+        std::vector<Particle*> pVector;
+
+        /** spatial variables */
+        Vec2f m_center;                 // x (t)
+//        Matrix2f m_Rotation;            // R (t)
+        Vec2f m_Velocity;               // x' (t)
+//        Vector2f m_spin;                // w (t)
+//
+//        /** points in body space */
+//        std::vector<Vec2f> m_points;    // p0
+//        std::vector<Vector2f> p_body;   // p0 body space
+//        std::vector<Vector2f> p_pos;
+//        std::vector<Vector2f> p_velocity;   // v_i (t)
+//
+        Vec2f m_Force;
+//        Vector2f m_Torque;
+        float m_Mass;
+        std::vector<Vec2f> m_Direction;     // r (t)
+//
+//        Matrix3f m_I;
+
 };
 
 
--- unified_makefile_project1/src/RigidObject.cpp
+++ unified_makefile_project1/src/RigidObject.cpp
@@ -1,47 +1,49 @@
 #include "RigidObject.h"
-#include <vector>
+//#include <vector>
 #include <GL/glut.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <Eigen/Dense>
-#include <Eigen/IterativeLinearSolvers>
-#include <gfx/vec2.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <Eigen/Dense>
+//#include <Eigen/IterativeLinearSolvers>
+//#include <gfx/vec2.h>
 
 RigidObject::RigidObject(Vec2f centerPoint) :
-    center(centerPoint), m_Velocity(Vec2f(0.0, 0.0)), m_Force(Vec2f(0.0, 0.0)), m_Mass(0.001),
-    m_Direction(std::vector<Vec2f>())
+    pCenter(new Particle(centerPoint, 1)), m_Velocity(Vec2f(0.0, 0.0)), m_Force(Vec2f(0.0, 0.0)),
+    m_Mass(0.01)//, m_Direction(std::vector<Vec2f>())
 {
-    computeBodySpace();
-    m_Direction.push_back(Vec2f(0.0, 0.0));
-    m_Direction.push_back(center);
+//    compute_body_space();
+//    m_Rotation = (0f, 0f, 0f, 0f);
+//
+//    m_Direction.push_back(Vec2f(0.0, 0.0));
+//    m_Direction.push_back(m_center);
+
+
+
+    // init particles in body space
+    float offset = 0.05;
+    pVector.push_back( new Particle(Vec2f(-offset, -offset), 1) );
+    pVector.push_back( new Particle(Vec2f(-offset, offset), 1) );
+    pVector.push_back( new Particle(Vec2f(offset, offset), 1) );
+    pVector.push_back( new Particle(Vec2f(offset, -offset), 1) );
+
 }
 
 RigidObject::~RigidObject(void) {}
 
-void RigidObject::computeBodySpace() {
-    float offset = 0.1;
-    points.clear();
-    points.push_back(Vec2f(offset, -offset)); //bottomright
-    points.push_back(Vec2f(offset, offset));  //topright
-    points.push_back(Vec2f(-offset, offset)); //topleft
-    points.push_back(Vec2f(-offset, -offset));//bottomleft
-
-}
-
 std::vector<Vec2f> RigidObject::get_state()
 {
     std::vector<Vec2f> state;
-    state.push_back(center);
+    state.push_back(m_center);
     state.push_back(m_Velocity);
-    m_Direction[0] = center;
+//    m_Direction[0] = m_center;
     return state;
 }
 
 void RigidObject::set_state(Vec2f pos, Vec2f vel)
 {
-    center = pos;
+    m_center = pos;
     m_Velocity = vel;
-    m_Direction[1] = pos;
+//    m_Direction[1] = pos;
 }
 
 void RigidObject::reset()
@@ -49,7 +51,7 @@ void RigidObject::reset()
 //    center = centerPoint;
     m_Velocity = Vec2f(0.0, 0.0);
     m_Force = Vec2f(0.0, 0.0);
-    m_Direction.clear();
+//    m_Direction.clear();
 }
 
 void RigidObject::clear_force()
@@ -66,18 +68,35 @@ std::vector<Vec2f> RigidObject::derive_eval() { // returns a vector of the veloc
 
 std::vector<Vec2f> RigidObject::get_points() {
     std::vector<Vec2f> worldPoints;
-    for (int i = 0; i < points.size(); i ++) {
-        worldPoints.push_back(Vec2f(center[0]+points[i][0], center[1]+points[i][1]));
+    for (int i = 0; i < pVector.size(); i ++) {
+        worldPoints.push_back(Vec2f(pCenter->m_Position[0] + pVector[i]->m_Position[0],
+                                    pCenter->m_Position[1] + pVector[i]->m_Position[1]));
     }
     return worldPoints;
 }
 
 void RigidObject::draw_object() {
     glColor3f(2.f, 0.7f, 4.f);
-    std::vector<Vec2f> worldPoints = get_points();
+//    std::vector<Vec2f> worldPoints = get_points();
     glBegin(GL_POLYGON);
-    for (int i = 0; i < points.size(); i ++) {
-        glVertex2f(worldPoints[i][0], worldPoints[i][1]);
+//    for (int i = 0; i < m_points.size(); i ++) {
+//        glVertex2f(worldPoints[i][0], worldPoints[i][1]);
+//    }
+    for (int i=0; i < pVector.size(); i++) {
+        glVertex2f(pCenter->m_Position[0] + pVector[i]->m_Position[0],
+                   pCenter->m_Position[1] + pVector[i]->m_Position[1]);
     }
     glEnd();
 }
+
+//void RigidObject::apply_transformation() {
+//    /**
+//     * R ' = w * R
+//     * r_i = R * m_points + center
+//     * v_i = w * R r_0i + v = w x (r_i - center) + v(t)
+//     *
+//     * torque_i = (r_i - center) X F_i
+//     *
+//     */
+//
+//}
